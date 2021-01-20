4713911fb56215362fa9c669c2d124fc34c92702
sivakumar.sundaramurthi@zohocorp.com <sivakumar.sundaramurthi@zohocorp.com>
2021-01-20 18:12:06 +0530

    [BUGFIX] boolean literal

--- tests/acceptance/inspector-test.js
+++ tests/acceptance/inspector-test.js
@@ -12,31 +12,32 @@ module('Acceptance | inspector', function(hooks) {
     let inspector = getService('template-inspector');
     currentURL('/');
     //application route
-    assert.dom('[data-test-title="addon-title"]').hasAttribute(locationAttribute, '1:3:1:0');
+    assert.dom('[data-test-title="addon-title"]').hasAttribute(locationAttribute);
 
     let l = document.querySelector('[data-test-title="addon-title"]').getAttribute('l');
     let fileName = await inspector.getFileInfo(l);
 
     assert.equal(fileName, 'dummy/templates/application.hbs:1:0', 'file location is not equal');
 
-    assert.dom('[data-test-title="application-route"]').hasAttribute(locationAttribute, '1:3:3:0');
+    assert.dom('[data-test-title="application-route"]').hasAttribute(locationAttribute);
 
     await click('[href="/classic-route"]');
 
     //classic route
-    assert.dom('[data-test-title="classic-route-title"]').hasAttribute(locationAttribute, '1:4:2:2');
-    assert.dom('[data-test-title="container"]').hasAttribute(locationAttribute, '1:4:7:0');
+    assert.dom('[data-test-title="classic-route-title"]').hasAttribute(locationAttribute);
+    assert.dom('[data-test-title="container"]').hasAttribute(locationAttribute);
 
     //classic component
-    assert.dom('[data-test-title="classic-component"]').hasAttribute(locationAttribute, '1:5:1:0')
+    assert.dom('[data-test-title="classic-component"]').hasAttribute(locationAttribute)
 
 
     //pod route
     await click('[href="/pod-route"]');
-    assert.dom('[data-test-title="pod-route-title"]').hasAttribute(locationAttribute, '1:2:2:2');
-    assert.dom('[data-test-title="container"]').hasAttribute(locationAttribute, '1:2:6:0');
+    assert.dom('[data-test-title="pod-route-title"]').hasAttribute(locationAttribute);
+    assert.dom('[data-test-title="container"]').hasAttribute(locationAttribute);
 
     //pod component
-    assert.dom('[data-test-title="pod-component"]').hasAttribute(locationAttribute, '1:1:1:0')
+    assert.dom('[data-test-title="pod-component"]').hasAttribute(locationAttribute)
+    assert.dom('[data-test-title="input-statement"]').hasAttribute(locationAttribute)
   });
 });
